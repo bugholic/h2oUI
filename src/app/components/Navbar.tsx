d10aadaaf3e8ee6309df087ee673c8e8ac8e4244
bugholic <masterav2001@gmail.com>
2024-10-03 17:47:42 +0530

    Updated Navbar with hamburger menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,39 +4,49 @@ import menu from "../../../assets/icons/menu.svg";
 import Button from "./Button";
 import Link from "next/link";
 
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+const handleMenuClick = () => {
+  setIsMenuOpen((prev) => !prev);
+};
+
 const Navbar = () => {
   return (
     <nav className="flex w-full">
       <Image alt="H2O" src={logo} width={100} />
-
-      <ul className="hidden items-center mx-3 w-full justify-center lg:flex">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/"> Who We Are </Link>
-        </li>
-        <li>
-          <Link href="/"> Market Applicaions </Link>
-        </li>
-        <li>
-          <Link href="/"> About Us </Link>
-        </li>
-        <li>
-          <Link href="/"> How It Works</Link>
-        </li>
-        <li>
-          <Link href="/contact-us">Contact Us</Link>
-        </li>
-      </ul>
-
+      {isMenuOpen && (
+        <ul className="block items-center bg-white mx-3 w-full justify-center lg:flex">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/"> Who We Are </Link>
+          </li>
+          <li>
+            <Link href="/"> Market Applicaions </Link>
+          </li>
+          <li>
+            <Link href="/"> About Us </Link>
+          </li>
+          <li>
+            <Link href="/"> How It Works</Link>
+          </li>
+          <li>
+            <Link href="/contact-us">Contact Us</Link>
+          </li>
+        </ul>
+      )}
       <div className="hidden lg:block">
         <Button
           bgcolor="bg-blue-600"
           textcolor="text-white"
           action="Let's Get Started"
         />
-        <button className="block bg-gray-300 rounded-full p-2 sm:hidden">
+
+        <button
+          onClick={() => handleMenuClick}
+          className="bg-gray-300 rounded-full p-2 sm:hidden"
+        >
           <Image src={menu} alt="menu" />
         </button>
       </div>
